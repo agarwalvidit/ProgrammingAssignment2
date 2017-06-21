30cd6f3bde58904861597e30fd9e18df6b4485c6
Hashpromt <vidit.bit@gmail.com>
2017-06-21 17:17:38 +0530

    Committing Assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function, `makeCacheMatrix` creates a special "vector", which is
+## really a list containing a function to
 
-## Write a short comment describing this function
+#   1.  set the value to Matrix
+#   2.  get the value of Matrix
+#   3.  set the value of the Inverse of Matrix
+#   4.  get the value of the Inverse of Matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) 
+  {
+    x <<- y
+    m <<- NULL
+  }     
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the Inverse of the special "vector"
+#created with the above function. However, it first checks to see if the
+#Matrix has already been calculated. If so, it `get`s the inverse from the
+#cache and skips the computation. Otherwise, it calculates the inverse of
+#the data and sets the value of the inverse in the cache via the `setInverse` function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) 
+  {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
